cadcd7a86b5901fd66fb9afbff8262e38004876e
minkiapps <minkiapps@gmail.com>
2022-04-28 09:26:23 +0200

    Add yoda module

---
+++ hos/yoda/src/main/js/default/app.js
@@ -0,0 +1,9 @@
+//gt2 pro has only 48KB RAM for each app
+export default {
+    onCreate() {
+        console.info("Application onCreate");
+    },
+    onDestroy() {
+        console.info("Application onDestroy");
+    }
+};
