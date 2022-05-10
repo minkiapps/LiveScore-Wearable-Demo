2fbbb168c4642f4fdbb911b677356e45507960ee
minkiapps <minkiapps@gmail.com>
2022-05-10 16:54:21 +0200

    Add watch3 module

---
+++ hos/watch3/src/ohosTest/java/com/minkiapps/hos/livescore/ExampleOhosTest.java
@@ -0,0 +1,14 @@
+package com.minkiapps.hos.livescore;
+
+import ohos.aafwk.ability.delegation.AbilityDelegatorRegistry;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleOhosTest {
+    @Test
+    public void testBundleName() {
+        final String actualBundleName = AbilityDelegatorRegistry.getArguments().getTestBundleName();
+        assertEquals("", actualBundleName);
+    }
+}
